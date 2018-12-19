1d64fe72b60efc1f26c58f34cb222e48d028f69f
Kevin Deisz <kevin.deisz@gmail.com>
2018-12-19 17:35:51 -0500

    Set up initial server

---
+++ getRedirect.js
@@ -0,0 +1,59 @@
+const https = require("https");
+
+const getCheckSuites = (owner, repo) => new Promise((resolve, reject) => {
+  const req = {
+    hostname: "api.github.com",
+    port: 443,
+    path: `/repos/${owner}/${repo}/commits/master/check-suites`,
+    method: "GET",
+    headers: {
+      Accept: "application/vnd.github.antiope-preview+json",
+      "User-Agent": "node"
+    }
+  };
+
+  https.get(req, resp => {
+    let data = "";
+
+    resp.on("data", chunk => { data += chunk; });
+    resp.on("error", error => reject(error));
+    resp.on("end", () => {
+      const parsed = JSON.parse(data);
+
+      if (resp.statusCode === 200) {
+        resolve(parsed.check_suites);
+      } else {
+        reject(parsed);
+      }
+    });
+  });
+});
+
+const getStatus = checkSuites => {
+  const matched = checkSuites.find(checkSuite => checkSuite.app.name === "GitHub Actions");
+  if (!matched) {
+    throw new Error("Could not find check suite named GitHub Actions");
+  }
+
+  return matched.conclusion;
+};
+
+const STATUS_COLORS = {
+  error: "red",
+  failure: "lightgrey",
+  pending: "yellow",
+  success: "green"
+};
+
+const getRedirectURL = status => {
+  const base = "https://img.shields.io/badge";
+  const query = "?logo=github&logoColor=white";
+
+  return `${base}/GitHub_Actions-${status}-${STATUS_COLORS[status]}.svg${query}`;
+};
+
+const getRedirect = (owner, repo) => (
+  getCheckSuites(owner, repo).then(getStatus).then(getRedirectURL)
+);
+
+module.exports = getRedirect;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+const getRedirect = require("./getRedirect");
+
+app.use((request, response, next) => {
+  console.log(`[${new Date().toUTCString()}] ${request.method} ${request.path}`);
+
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+
+  next();
+});
+
+app.get("/:owner/:repo", (request, response) => {
+  getRedirect(request.params.owner, request.params.repo)
+    .then(redirect => {
+      response.header("Cache-Control", "no-cache");
+      response.redirect(redirect);
+    })
+    .catch(error => {
+      if (error.message.startsWith("Could not find")) {
+        response.status(404).send("Not found");
+      } else {
+        response.status(500).send(error.message);
+      }
+    });
+});
+
+app.listen(8080, () => console.log("Listening on port 8080..."));
